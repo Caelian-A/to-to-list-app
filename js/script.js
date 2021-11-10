372125f2acb61c5dc3df9ba934b31d3cef1b2f1a
Caelian-A <92312529+Caelian-A@users.noreply.github.com>
2021-11-10 18:28:05 +0000

    Update following tutor feedback

--- js/script.js
+++ js/script.js
@@ -1,62 +1,31 @@
 //adding a new item to the list
-function newItem(){
+function newItem() {
     let li = $('<li></li>');
     let inputValue = $('#input').val();
     li.append(inputValue);
 
-    if (inputValue === ''){
+    if (inputValue === '') {
         alert('Field cannot be empty');
-        } else {
+    } else {
         $('#list').append(li);
     }
 
-//Strikethrough an item
+    //Strikethrough an item
     function strikeThrough() {
-    li.toggleClass('strike');
+        li.toggleClass('strike');
     }
 
     li.on('dblclick', strikeThrough);
 
-//Delete item button
-    let deleteButton = $('<deleteButton></deleteButton>');
+    //Delete item button
+    let deleteButton = $('<button></button>');
     deleteButton.append(document.createTextNode('X'));
     li.append(deleteButton);
 
     deleteButton.on('click', deleteListItem);
-    function deleteListItem(){
-    li.addClass('delete')
+    function deleteListItem() {
+        li.addClass('delete')
     }
-//sorting the list
+    //sorting the list
     $('#list').sortable();
-    }
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+}
